99c2924ece72484bad59c0d69d1206226b7cd0cb
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-20 21:43:34 +0200

    Added credentials to install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
+#!/usr/bin/bash
 mkdir /usr/bin
 
 cp ./main.py /usr/bin/github
-touch /usr/bin/data.env
+touch /usr/bin/.env
 
-cd /usr/bin/
+cd /usr/bin/ || exit
 
 pip install gitpython pygithub python-dotenv
 
-echo "Successfully installed github-cli! Please paste ACCESS_TOKEN=<your access token> into ~/bin/data.env"
+echo "Successfully installed github-cli!"
+
+printf "Please enter your GitHub credentials "
+
+read -p 'Username: ' username
+read -sp 'Access Token: ' access_token
+
+echo
+echo "USERNAME=$username" > .env
+echo "\n" > data.env
+echo "ACCESS_TOKEN=$access_token" >> .env
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-username = 'Baumistlustig'
+username = os.getenv('USERNAME')
 
 # Connect to GitHub Account
 g = Github(os.getenv('ACCESS_TOKEN'))
