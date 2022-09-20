206dc4c8496ce7668302fa0c1b5be1486dd1f55a
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-20 20:53:18 +0200

    Fixed installation

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
-cp ./main.py ~/bin/github
-touch ~/bin/data.env
+mkdir /usr/bin
 
-pip install git github dotenv
+cp ./main.py /usr/bin/github
+touch /usr/bin/data.env
+
+cd /usr/bin/
+
+pip install gitpython pygithub python-dotenv
 
 echo "Successfully installed github-cli! Please paste ACCESS_TOKEN=<your access token> into ~/bin/data.env"
